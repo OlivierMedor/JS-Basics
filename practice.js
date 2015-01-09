53707685f6d721c1f4387700a25d6ec0ec4d3fb6
Oliver <oliviermedor@yahoo.com>
2015-01-09 18:45:16 -0500

    new

--- practice.js
+++ practice.js
@@ -5,7 +5,14 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+var isTyler = function(name) {
+  if (name === "Tyler"){
+    return true;
+
+  }else{
+    return false;
+  }
+}
 
 
 //Next problem
@@ -15,7 +22,10 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+var getName = function() {
+  username = prompt('Name please');
+  return username
+}
 
 
 //Next Problem
@@ -25,7 +35,10 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+var greetings = function(getName) { 
+  alert('Welcome ' + getName);
+}
+ 
 
 
 //Next problem
@@ -36,8 +49,15 @@ var name = 'Tyler';
 //loop over all the arguments the function received, add them all together, then return the result.
 //Save the result of the return value from your adder function, then alert "The Total Number was " + the number that was
 //returned from adder.
+var adder = function() {
+  var totalSum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    totalSum = totalSum + arguments[i];
+  };
+  return totalSum;
 
-  //Code Here
+}
+ 
 
 
 //Next Problem
@@ -46,7 +66,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+argumuments are what is given to a function in the parens to opperate while being created. Parameters are what a function uses in the parens after the function is already created
 
 
 //Next problem
@@ -56,7 +76,7 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+Falsy arguments are valuse such as false, 0, NaN, Null, undefined, empty string. To find if someething is falsy, we can use comparitor opperators.
 
 
 
@@ -66,13 +86,17 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
+var myName = function() {
+  var name = prompt('name please');
+  return name;
+}
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName();
 
 //Now alert the result of invoking newMyName
+alert(newMyName);
 
 
 
@@ -82,10 +106,15 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
-
+var outerFn = function(){
+   return function (){
+    return prompt("name?");
+   }
+  
+};
 //Now save the result of invoking outerFn into a variable called innerFn.
-
+var innerFn = outerFn();
   //Code Here
 
 //Now invoke innerFn.
+innerFn();
